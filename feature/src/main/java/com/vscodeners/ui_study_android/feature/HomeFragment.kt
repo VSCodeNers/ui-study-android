23bd661f7f955d35057e19b8d9e444aa01c3fdc1
jung0115 <jungim7490@gmail.com>
2024-08-18 02:33:33 +0900

    [FEAT] #14 - week04: Navigation Graph 세팅

---
+++ feature/src/main/java/com/vscodeners/ui_study_android/feature/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.vscodeners.ui_study_android.feature
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.vscodeners.ui_study_android.feature.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private var navController: NavController? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?{
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = view.findNavController()
+
+        binding.btnLogin.setOnClickListener {
+            navController?.navigate(R.id.action_navigate_fragment_to_login)
+        }
+    }
+}
